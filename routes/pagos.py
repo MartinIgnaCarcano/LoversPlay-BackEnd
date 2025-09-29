849b95cefe12b83f18235b107c81bd8951628e1c
MartinIgnaCarcano <149337574+MartinIgnaCarcano@users.noreply.github.com>
2025-09-29 19:24:04 -0300

    Update pagos.py

--- routes/pagos.py
+++ routes/pagos.py
@@ -4,5 +4,5 @@ from flask import Blueprint, request, jsonify
 
 pagos_bp = Blueprint("pagos", __name__, url_prefix="/api/pagos")
 
-sdk = mercadopago.SDK("APP_USR-6608167090676875-091814-964957d986e21eee913f06709c51abeb-2611950632")
+sdk = mercadopago.SDK("")
 
